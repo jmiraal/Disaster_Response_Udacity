4aa7680db9ef778f618c4fc7e425c65150dfb1fd
Jesus Mira Aldao <jesus.mira1@hotmail.com>
2020-04-28 20:14:35 +0200

    Initial Commit

---
+++ data/process_data.py
@@ -0,0 +1,93 @@
+import sys
+import pandas as pd
+import numpy as np
+import sqlalchemy as db
+
+
+
+def load_data(messages_filepath, categories_filepath):
+    # read messages file
+    messages_df = pd.read_csv(messages_filepath)
+    # read categories file
+    categories_df = pd.read_csv(categories_filepath)
+    # merge the two files
+    df = messages_df.merge(categories_df, on=['id'])
+    return df
+
+
+def clean_data(df):
+    # create a dataframe of the 36 individual category columns
+    categories = df.categories.str.split(pat = ';', expand = True)
+    # select the first row of the categories dataframe
+    row = categories.iloc[0,:]
+
+    # use this row to extract a list of new column names for categories.
+    category_colnames = row.str.slice(start = 0, stop = -2)
+    categories.columns = category_colnames
+    
+    # convert category values to just numbers 0 or 1
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str.slice(start = -1)
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+        
+    # drop the original categories column from `df`
+    df = df.drop(columns = ['categories'])
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df,categories], axis=1)
+    
+    # drop duplicates
+    df = df.drop_duplicates()
+    
+    # drop column child_alone. We don't have any messages of this label
+    df = df.drop(columns = ['child_alone'])
+    
+    # drop all the rows where related = 2.
+    df = df[~(df['related'] == 2)]
+    
+    # drop the rows where the length of the message is samaller than 27 chars
+    df['len'] = df.message.apply(len)
+    df = df[df['len'] >= 27]
+    
+    return df
+
+def save_data(df, database_filename):
+    # open a connection with the database
+    engine = db.create_engine('sqlite:///' + database_filename)
+    connection = engine.connect()
+    # save the dataframe df into the table messages
+    df.to_sql('messages', engine, index=False, if_exists='replace')
+    #close the connection with database
+    return connection.close()
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
---
+++ models/train_classifier.py
@@ -0,0 +1,434 @@
+import sys
+import nltk
+from nltk.corpus import stopwords
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
+
+import re
+import time
+import numpy as np
+import pandas as pd
+from pandas.api.types import CategoricalDtype
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import sqlalchemy as db
+
+from sklearn.metrics import confusion_matrix, multilabel_confusion_matrix, classification_report
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.datasets import make_multilabel_classification
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import classification_report
+from sklearn.linear_model import SGDClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import LinearSVC
+
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+import pickle
+
+import random
+
+
+labels = []
+
+def load_data(database_filepath):
+    engine = db.create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('messages', engine)
+    X = df[['message', 'genre']]
+    Y = df.drop(columns = ['id', 'message', 'original', 'genre']).values
+    labels = df.columns[4:-2]
+    return X, Y, labels
+
+
+def tokenize(text):
+    # patterns to detect url'sand users
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    #hashtag_regex = '#[A-Za-z0-9]*'
+    user_regex = '(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9-_]+)'
+    
+    # change urls by the word "urlplaceholder"
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+    
+    detected_users = re.findall(user_regex, text)
+    for users in detected_users:
+        text = text.replace(users, "userplaceholder")
+    
+    
+    # replace all the chars not in a-z, A-Z or 0-9 by " "
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text)
+    
+    # tokenize the text and drop the the stop words in Enghish
+    tokens = word_tokenize(text)
+    tokens = [w for w in tokens if w not in stopwords.words("english")]
+    
+    # initialize the lemmatizer
+    lemmatizer = WordNetLemmatizer()
+
+    # lemmatize each token and return a list of lower lematized tokens
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+
+class GPEExtractor(BaseEstimator, TransformerMixin):
+
+    '''
+       Tranformer to detect if ther is a GPE into the message
+    '''
+
+    def gpe_extractor(self, text):
+        # tokenize by sentences
+        sentence_list = nltk.sent_tokenize(text)
+        
+        for sentence in sentence_list:
+            # tokenize each sentence into words and tag part of speech
+            tree = nltk.ne_chunk(nltk.pos_tag(word_tokenize(sentence)))
+            if ('GPE' in str(tree)):
+                
+                return True
+            else:
+                return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        # apply gpe_extractor function to all values in X
+        X_tagged = pd.Series(X).apply(self.gpe_extractor)
+        return pd.DataFrame(X_tagged)
+  
+  
+class PersonExtractor(BaseEstimator, TransformerMixin):
+
+    '''
+       Tranformer to detect if a person in named into the message
+    '''
+
+    def person_extractor(self, text):
+        # tokenize by sentences
+        sentence_list = nltk.sent_tokenize(text)
+        
+        for sentence in sentence_list:
+            # tokenize each sentence into words and tag part of speech
+            tree = nltk.ne_chunk(nltk.pos_tag(word_tokenize(sentence)))
+            if ('PERSON' in str(tree)):
+                return True
+            else:
+                
+                return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        # apply person_extractor function to all values in X
+        X_tagged = pd.Series(X).apply(self.person_extractor)
+        return pd.DataFrame(X_tagged)
+
+
+class OrganizationExtractor(BaseEstimator, TransformerMixin):
+    '''
+       Tranformer to detect if there is an Organization in the message
+    '''
+
+    def organization_extractor(self, text):
+        # tokenize by sentences
+        sentence_list = nltk.sent_tokenize(text)
+        
+        for sentence in sentence_list:
+            # tokenize each sentence into words and tag part of speech
+            tree = nltk.ne_chunk(nltk.pos_tag(word_tokenize(sentence)))
+            if ('ORGANIZATION' in str(tree)):
+                return True
+            else:
+                return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        # apply organization_extractor function to all values in X
+        X_tagged = pd.Series(X).apply(self.organization_extractor)
+        return pd.DataFrame(X_tagged)
+        
+        
+
+class Reshape_Array(BaseEstimator, TransformerMixin):
+    '''
+        It converts a Series or a Dataframe into a matrix with two dimensions.
+    '''
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        return X.values.reshape(-1, 1)
+
+
+class ColumnSelector(BaseEstimator, TransformerMixin):
+    '''
+       Transformer to select a column where to  apply the transformation
+    '''
+
+    def __init__(self, column):
+        self.column = column
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        return X[self.column]
+
+
+
+def model_pipeline_rf_complete():
+    '''
+       define a pipeline for random forest with the features:
+       tfidf: ftidf features of the column message.
+       person: message makes refference to a person?
+       organization: message makes refference to an organization?
+       gpe: message makes refference to a geopolitical entity?
+       len: longitude of the message
+       genre: categorical variable: news, direct or social.
+    '''
+    pipeline = Pipeline([
+        ('features', FeatureUnion(
+             transformer_list=[
+                 # tfidf features over column message
+                 ('text_pipeline', Pipeline([
+                     ("selector", ColumnSelector(column="message")),
+                     ('vect', CountVectorizer(tokenizer=tokenize, max_features = 5000)),
+                     ('tfidf', TfidfTransformer())
+                     ])
+                  ),
+                 # search words features over columns message
+                  ('word_pipeline', Pipeline([
+                        ("selector", ColumnSelector(column="message")),
+                         ('word_features', FeatureUnion([
+                             ('person', PersonExtractor()),
+                             ('organization', OrganizationExtractor()),
+                             ('gpe', GPEExtractor())
+                             ])
+                         )
+             
+                   ])),
+                 # genre features. OneHotEncoding of column genre
+                   ('len_pipeline', Pipeline([
+                       ('selector', ColumnSelector(column="len")),
+                       ('reshape', Reshape_Array()),
+                       ('len', StandardScaler())
+                       ])
+                   ),
+                 # genre features. OneHotEncoding of column genre
+                   ('genre_pipeline', Pipeline([
+                       ('selector', ColumnSelector(column="genre")),
+                       ('reshape', Reshape_Array()),
+                       ('genre', OneHotEncoder())
+                       ])
+                   )
+            ]
+        )),
+        # apply Random Forests method
+        ('multi_clf', MultiOutputClassifier(RandomForestClassifier(n_estimators=35, min_samples_split = 3)))
+    ])
+    return pipeline
+
+
+def model_pipeline_svc_gs():
+    '''
+       Define a LinearSVC pipeline
+    '''    
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize, max_features = 5000)),
+        ('tfidf', TfidfTransformer()),
+        ('multi_clf', MultiOutputClassifier(LinearSVC(C=1)))
+    ])
+    return pipeline
+    
+
+
+def model_pipeline_rf_gs():
+    '''
+       Define a Random Forests pipeline
+    '''
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize, max_features = 5000)),
+        ('tfidf', TfidfTransformer()),
+        ('multi_clf', MultiOutputClassifier(RandomForestClassifier(n_estimators=35, min_samples_split = 3)))
+    ])
+    return pipeline
+    
+    
+def show_plot(df):
+    """
+    print different plots depending of the stat option
+    Args:
+    (int) stat_option - Main program option ("global", "trip table", "correlation", "percentage")
+    """
+    aux_results = df[df.value == 1].sort_values(by = 'recall', 
+                                                ascending = False)
+    aux_results = aux_results.set_index('label')
+    aux_results.plot(kind= 'bar' ,
+                     y = ['f1-score', 'recall', 'precision', 'support'],
+                     secondary_y = 'support',
+                     use_index = True,
+                     rot= 90,
+                     width = 0.8)
+    plt.show()
+
+
+def display_results(y_test, y_pred, labels, printout = False):
+    '''
+    USAGE: 
+        It prints the resulst of sklearn.metrics.classification_report.
+        
+    INPUT
+        y_test: Lists with the real values for the response variables
+        y_pred: Lists with the predicted values for the response variables.
+        labels: Names or labels for the response variables
+        printout: False: returns a dataframe with the sesult values for each label
+                  True: returns a printout.
+    OUTPUT
+        result: A dataframe with the values of precision, reacall, f1-score, support,
+                accuracy for each label and value (0 or 1)
+    '''
+    
+    i= 0
+    
+    if printout == False:
+        data = []
+        result = pd.DataFrame(columns = ['label', 'value', 'precision', 'recall', 'f1-score', 'support', 'accuracy'])
+        for label in labels:
+        
+            class_report = classification_report(y_test[:,i], y_pred[:,i], output_dict = True)
+            
+            data = [label , 0, 
+                    class_report['0']['precision'], 
+                    class_report['0']['recall'],
+                    class_report['0']['f1-score'],
+                    class_report['0']['support'], 
+                    class_report['accuracy']]
+
+            s = pd.Series(data, index=result.columns)
+            result = result.append(s, ignore_index=True)
+            data = [label , 1, 
+                    class_report['1']['precision'], 
+                    class_report['1']['recall'],
+                    class_report['1']['f1-score'],
+                    class_report['1']['support'], 
+                    class_report['accuracy']]
+
+            s = pd.Series(data, index=result.columns)
+            result = result.append(s, ignore_index=True)
+            i += 1
+        return result
+        
+    else:
+        for label in labels:
+            class_report = classification_report(y_test[:,i], y_pred[:,i])
+            accuracy = (y_pred[:,i] == y_test[:,i]).mean()
+            print("Label: ", label)
+            print("Confusion Matrix:\n", class_report)
+            print("Accuracy:", accuracy)
+            i += 1
+        
+        
+
+def elapsed_time_decorator(func):
+    '''
+        We define a function to define a decorator to print the time took in executing a function
+    '''
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        elapsed_time = time.time() - start_time
+        print('Elapsed Time: ', time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
+        return result
+    return wrapper
+
+
+@elapsed_time_decorator
+def model_fit_pred(model, X_train, X_test, Y_train):
+    '''
+    USAGE 
+           define a function to fit a model and make the predictions.
+           It will print the time elapsed in the process
+    INPUT
+           model: model we wanted to train
+           X_train, Y_train: data to train the model
+           X_test: data to evaluate the model         
+    OUTPUT
+           Y_pred: labels predicted in the X_test 
+    '''
+    print('Training model...')
+    model.fit(X_train, Y_train)
+    print('Evaluating model...')
+    Y_pred = model.predict(X_test)
+    return Y_pred
+    
+def save_model(model, model_filepath):
+    ''' 
+    Saving model's best_estimator_ using pickle
+    '''
+    pickle.dump(model.get_params(), open(model_filepath, 'wb'))
+
+
+def main():
+    # check the correct number of arguments
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        # load the database data
+        X, Y, labels = load_data(database_filepath)
+        
+        # split the data in train and test parts
+        X_train_1, X_test_1, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        X_train = X_train_1.message.values
+        X_test = X_test_1.message.values
+        
+        print('Building model...')
+        model_svc_gs = model_pipeline_svc_gs()
+        
+        # fit and predict with the model defined in model_svc_gs
+        Y_pred = model_fit_pred(model_svc_gs, X_train, X_test, Y_train)
+        
+        # save and show in a plot the results for precision, recall, f1-score
+        results_svc_gs = display_results(Y_test, Y_pred, labels)
+        show_plot(results_svc_gs)
+        results_svc_gs.to_csv("results_svc_gs.csv", index=False)
+        
+        # save the model in a fil to used it later
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model_svc_gs, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
